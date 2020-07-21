d748df575f070d367aae1afa8c5269338b212396
dupuyr <dupuyr@tcd.ie>
2020-07-21 19:00:14 +0100

    Tidied up code and file containing all d3 repos

--- GH.java
+++ GH.java
@@ -1,27 +1,21 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
+
 public class GH {
 
-public static void main(String[] args) throws IOException{
-getRequest();
+ public static void main(String[] args) throws IOException{
+  getRequest();
 
-}
-
- public static URL getUrl () throws IOException{
-   URL url = new URL("https://api.github.com/users/d3/repos");
-   return url;
  }
 
  public static void getRequest() throws IOException{
-   HttpURLConnection c = (HttpURLConnection) getUrl().openConnection();
-   c.setRequestMethod("GET");
-   toFile(c);
- }
 
- public static void toFile(HttpURLConnection h) throws IOException{
-   BufferedReader in = new BufferedReader(new InputStreamReader(h.getInputStream()));
-   BufferedWriter f = new BufferedWriter(new FileWriter(new File("test.txt")));
+   URL u = new URL("https://api.github.com/users/d3/repos");
+   HttpURLConnection c = (HttpURLConnection) u.openConnection();
+   c.setRequestMethod("GET");
+   BufferedReader in = new BufferedReader(new InputStreamReader(c.getInputStream()));
+   BufferedWriter f = new BufferedWriter(new FileWriter(new File("d3.txt")));
    String inputLine;
    StringBuffer response = new StringBuffer();
     while ((inputLine = in.readLine()) != null) {
@@ -31,5 +25,6 @@ getRequest();
         in.close();
         f.write(s);
         f.close();
+  }
+
  }
-}
