4d63b5f82a276d6475a5235c0f33a39740831ead
dupuyr <dupuyr@tcd.ie>
2020-07-21 18:37:26 +0100

    Basic code that GETs all respositories owned by d3 and saves to file.

--- GH.java
+++ GH.java
@@ -1,22 +1,35 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 public class GH {
 
-public static void main(String[] args){
-
+public static void main(String[] args) throws IOException{
+getRequest();
 
 }
 
- public URL getUrl () throws IOException{
-   URL url = new URL("https://api.github.com/repos/d3");
+ public static URL getUrl () throws IOException{
+   URL url = new URL("https://api.github.com/users/d3/repos");
    return url;
  }
 
- public void getRequest() throws Exception{
+ public static void getRequest() throws IOException{
    HttpURLConnection c = (HttpURLConnection) getUrl().openConnection();
    c.setRequestMethod("GET");
-   BufferedReader in = new BufferedReader (new InputStreamReader(con.getInputStream()));
+   toFile(c);
+ }
+
+ public static void toFile(HttpURLConnection h) throws IOException{
+   BufferedReader in = new BufferedReader(new InputStreamReader(h.getInputStream()));
+   BufferedWriter f = new BufferedWriter(new FileWriter(new File("test.txt")));
    String inputLine;
    StringBuffer response = new StringBuffer();
+    while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        String s = response.toString();
+        in.close();
+        f.write(s);
+        f.close();
  }
 }
