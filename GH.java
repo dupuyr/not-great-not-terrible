018a9b35e00cf38a00712e5fd881d7ad004cd4b1
dupuyr <dupuyr@tcd.ie>
2020-08-14 13:58:03 +0100

    Getting all contributors

--- GH.java
+++ GH.java
@@ -5,11 +5,12 @@ import java.util.*;
 public class GH {
 
  public static void main(String[] args) throws IOException{
-  getRequest();
+  //getRequest();
+  getContributors();
 
  }
 
- public static void getRequest() throws IOException{
+/* public static void getRequest() throws IOException{
 
    URL u = new URL("https://api.github.com/users/d3/repos");
    HttpURLConnection c = (HttpURLConnection) u.openConnection();
@@ -25,6 +26,26 @@ public class GH {
         in.close();
         f.write(s);
         f.close();
+  }*/
+
+   public static void getContributors() throws IOException{
+
+       URL u2 = new URL("https://api.github.com/users/d3/repos/d3/stats/contributors");
+       HttpURLConnection c2 = (HttpURLConnection) u2.openConnection();
+       c2.setRequestMethod("GET");
+       BufferedReader in2 = new BufferedReader(new InputStreamReader(c2.getInputStream()));
+       BufferedWriter f2 = new BufferedWriter(new FileWriter(new File("d3cons.txt")));
+       String inputLine;
+       StringBuffer response = new StringBuffer();
+        while ((inputLine = in2.readLine()) != null) {
+                response.append(inputLine);
+            }
+            String s = response.toString();
+            in2.close();
+            f2.write(s);
+            f2.close();
+
+
   }
 
  }
