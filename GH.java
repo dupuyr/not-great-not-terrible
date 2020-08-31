b4427a3308fe88a51b698d441b698cbe39728af1
dupuyr <dupuyr@tcd.ie>
2020-08-31 23:43:20 +0100

    final bits

--- GH.java
+++ GH.java
@@ -33,15 +33,19 @@ public class GH {
 
   public Cons() {
    this.lines = 0;
+   this.name = "";
+
   }
 
   //converting unix to readable date for each month
   public static Month convertMonth(Month m) {
 
-   Instant i = Instant.ofEpochSecond(m.month * 1000);
-   Date d = Date.from(i);
-   SimpleDateFormat formatter = new SimpleDateFormat("MM/yyyy");
+
+   Date d = new Date(m.month * 1000L);
+   SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+   formatter.setTimeZone(TimeZone.getTimeZone("GMT+0"));
    String dt = formatter.format(d);
+   System.out.println(dt);
    m.date = dt;
    return m;
   }
@@ -66,7 +70,9 @@ public class GH {
 
   }
 
+  public Repos() {
 
+  }
  }
 
  public static class Month {
@@ -108,11 +114,10 @@ public class GH {
   getCons();
   calculateLines();
   getExactMonth();
-  createCSVFiles();
   writeToCSV();
 
  }
-
+//get repo names
  public static void getRepos() throws IOException, ParseException {
 
   URL url = new URL("https://api.github.com/users/d3/repos");
@@ -134,12 +139,13 @@ public class GH {
   for (Object value : j) {
    o = (JSONObject) value;
    String s = (String) o.get("name");
+   System.out.println(s);
    repos.add(s);
   }
 
 
  }
-
+// get stats for repos
  public static void getStats() throws IOException, ParseException {
 
   for (String repo : repos) {
@@ -148,7 +154,7 @@ public class GH {
 
  }
 
-
+//parse stats
  public static void parseArrays(String repo) throws IOException, ParseException {
 
   URL u;
@@ -171,7 +177,7 @@ public class GH {
 
 
  }
-
+//add stats to list
  public static void getRepoStats() {
 
   for (String re : repos) {
@@ -183,7 +189,7 @@ public class GH {
   }
 
  }
-
+// get contributors for each repo
  public static void getCons() {
 
   for (Repos r : repoStats) {
@@ -194,7 +200,7 @@ public class GH {
   }
 
  }
-
+//calculate lines for each contributor for each month
  public static void calculateLines() {
   long code = 0;
   long m = 0;
@@ -208,7 +214,7 @@ public class GH {
     } else if (m == 2419200) {
      Month mth = new Month(code, (long) j.get("w"));
      c.months.add(c.convertMonth(mth));
-     //System.out.println( c.months.get(0)+ " " + code);
+     System.out.println(c.months.get(0).date + " " + code);
      code = 0;
      m = 0;
     }
@@ -218,7 +224,7 @@ public class GH {
   }
   assignToRepo();
  }
-
+//calculate top contributor for each month
  public static void assignToRepo() {
 
   for (Cons c : contributors) {
@@ -228,7 +234,7 @@ public class GH {
 
  }
 
- public static void getExactMonth() throws IOException {
+ public static void getExactMonth() {
 
   for (Repos r : repoStats) {
    for (Month m : r.cons.get(0).months) {
@@ -239,7 +245,7 @@ public class GH {
  }
 
  public static void getTopCons(String m, Repos r) {
-
+  System.out.println(m);
   long top = 0;
   long second = 0;
   long third = 0;
@@ -255,16 +261,19 @@ public class GH {
      if (mt.codeLines > top) {
       top = mt.codeLines;
       c1.lines = mt.codeLines;
+      System.out.println(c1.lines);
       c1.name = c.name;
 
      } else if (mt.codeLines < top && mt.codeLines > second) {
       second = mt.codeLines;
       c2.lines = mt.codeLines;
+      System.out.println(c2.lines);
       c2.name = c.name;
 
      } else if (mt.codeLines < top && mt.codeLines < second && mt.codeLines > third) {
       third = mt.codeLines;
       c3.lines = mt.codeLines;
+      System.out.println(c3.lines);
       c3.name = c.name;
 
 
@@ -277,41 +286,41 @@ public class GH {
 
 
  }
+//write code to csv file
+ public static void writeToCSV() throws IOException {
 
- public static void createCSVFiles(){
+  File f = new File("C:\\Users\\maxri\\OneDrive\\Desktop\\CSV\\d3.csv");
 
-  for(String r: repos){
-    File f = new File("C:\\Users\\maxri\\OneDrive\\Desktop\\CSV\\" + r + ".csv");
-  }
+  FileWriter output = new FileWriter(f);
+  CSVWriter w = new CSVWriter(output);
 
- }
+  try {
 
- public static void writeToCSV(){
-   for(Repos r : repoStats){
-    try {
-
-     FileWriter output = new FileWriter("C:\\Users\\maxri\\OneDrive\\Desktop\\CSV" + r.rep + ".csv");
-     CSVWriter w = new CSVWriter(output);
-
-     String[] header = { "Month", "Name", "Lines" };
-     w.writeNext(header);
-     for(Month m : r.topMonth){
-      String[] data = { m.date, m.t.name ,Long.toString(m.t.lines)};
-      w.writeNext(data);
-      String[] data2 = {m.date, m.s.name, Long.toString(m.s.lines)};
-      w.writeNext(data2);
-      String[] data3 = {m.date,m.th.name, Long.toString(m.th.lines)};
-      w.writeNext(data3);
-     }
-
-     w.close();
-    }
-    catch (IOException e) {
-     e.printStackTrace();
+   String[] header = {"Month", "Name", "Lines"};
+   w.writeNext(header);
+   Repos s = new Repos();
+   for (Repos r : repoStats) {
+    if (r.rep.equals("d3")) {
+      s = r;
     }
    }
+   for (Month m : s.topMonth) {
+    String[] data = {m.date, m.t.name, Long.toString(m.t.lines)};
+    w.writeNext(data);
+    String[] data2 = {m.date, m.s.name, Long.toString(m.s.lines)};
+    w.writeNext(data2);
+    String[] data3 = {m.date, m.th.name, Long.toString(m.th.lines)};
+    w.writeNext(data3);
+   }
+  } finally {
+   w.close();
+
+  }
  }
 }
+
+
+
 
 
 
